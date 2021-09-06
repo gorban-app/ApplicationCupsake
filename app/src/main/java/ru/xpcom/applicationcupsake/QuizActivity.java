df5658119f7e34daa08f77aa40691f18c3c3b4ba
gorban-am <xpcom@ya.ru>
2021-09-06 11:09:18 +0300

    add file and code

--- app/src/main/java/ru/xpcom/applicationcupsake/QuizActivity.java
+++ app/src/main/java/ru/xpcom/applicationcupsake/QuizActivity.java
@@ -8,12 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button btnTrue;
     private Button btnFalse;
@@ -36,22 +39,24 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         uiComponent();
-        onClickButton();
         updateQuestion();
+        btnVisibleAndInvisible();
+        Log.d(TAG, "onCreate(Bundle) called");
     }
 
-    private void onClickButton() {
-        btnTrue.setOnClickListener(v -> showToast(checkAnswer(true)));
-        btnFalse.setOnClickListener(v -> showToast(checkAnswer(false)));
-        btnNext.setOnClickListener(v -> {
+    public void onClickButton(View view){
+        if(view.getId() == R.id.true_btn) {
+            showToast(checkAnswer(true));
+        } else if(view.getId() == R.id.false_btn){
+            showToast(checkAnswer(false));
+        } else if(view.getId() == R.id.next_btn) {
             currentIndex = (currentIndex + 1) % questionsBank.length;
             updateQuestion();
-        });
-        btnPrev.setOnClickListener(v -> {
-            if(currentIndex == 0) return;
+        } else if(view.getId() == R.id.prev_btn) {
             currentIndex = (currentIndex - 1) % questionsBank.length;
             updateQuestion();
-        });
+        }
+        btnVisibleAndInvisible();
     }
 
     private int checkAnswer(boolean userPressedTrue) {
@@ -73,7 +78,47 @@ public class QuizActivity extends AppCompatActivity {
         txtQuestion = findViewById(R.id.question_text);
     }
 
+    private void btnVisibleAndInvisible() {
+        if(currentIndex == 0) btnPrev.setVisibility(View.INVISIBLE);
+        else if(currentIndex == 1) btnNext.setVisibility(View.INVISIBLE);
+        else {
+            btnPrev.setVisibility(View.VISIBLE);
+            btnNext.setVisibility(View.VISIBLE);
+        }
+    }
+
     private void showToast(int messageToast) {
         Toast.makeText(getApplicationContext(), messageToast, Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
 }
